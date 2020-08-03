2855a75f8ce7bd16ea550bae5f7c1aa98d2ace62
fanjinyang <fanjinyang@coohua.com>
2020-08-03 21:10:31 +0800

    add

---
+++ src/main/java/com/sunliwei/practicer/PracticerApplication.java
@@ -0,0 +1,13 @@
+package com.sunliwei.practicer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunliwei/practicer/PracticerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunliwei.practicer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
